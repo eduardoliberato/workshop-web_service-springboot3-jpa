df562ea5ef4484a466b81a06a26a483809f41d0f
eduardoliberato <eduardo.d.liberato@gmail.com>
2023-09-29 17:03:44 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController //This tells Spring that this code describes an endpoint that should be made available over the web 
-@RequestMapping(value = "/users")// tells Spring to use our findAll() method to answer requests that get sent to the http://localhost:8080/users address
+@RequestMapping(value = "/users/")// tells Spring to use our findAll() method to answer requests that get sent to the http://localhost:8080/users address
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
 	
 	//use the class "User" as the type 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@maira", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		//ResponseEntity.ok() = response of success of the http , body of the answer (the content) 
 }
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service //to register a service, as a component of the spring
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository; //to create dependency
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); //will return a obj of the type User, that be inside the optional
+}
+}
