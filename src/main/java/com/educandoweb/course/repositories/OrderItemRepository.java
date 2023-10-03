72f23486ad60d5eb33949760d63dc60bace8f0b1
eduardoliberato <eduardo.d.liberato@gmail.com>
2023-10-02 21:38:42 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -2,9 +2,9 @@ package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.OrderItem;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 	
 	//is not necessary to implement the interface, because the JpaRepository already has a standard implementation
 	//This UserRepository can access the database and save information there, this is his function 
